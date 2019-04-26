4c79cebfda62aea747ce4a3fe8af0c3f31de72e7
Pietro Zuntini - Desktop <pietrozuntini@gmail.com>
2019-04-25 21:30:55 -0300

    Estados: Distrito Federal High

---
+++ amchats4/geodata/estados/dfHigh.js
@@ -0,0 +1,23 @@
+am4internal_webpackJsonp(["35f4"], {
+    pqZ3:function(e, t, o) {
+        "use strict";
+        Object.defineProperty(t, "__esModule", {
+            value: !0
+        }
+        );
+        window.am4geodata_brazilHigh= {
+            type:"FeatureCollection", features:[ {
+                type:"Feature", geometry: {
+                    type: "Polygon", coordinates: [[[-47.3051, -16.039], [-48.2464, -16.038], [-48.2796, -15.8356], [-48.2208, -15.722], [-48.1967, -15.4903], [-47.4307, -15.4911], [-47.3136, -15.614], [-47.3098, -15.7514], [-47.3478, -15.8249], [-47.3614, -15.9764], [-47.3051, -16.039]]]
+                }
+                , properties: {
+                    name: "Distrito Federal", id: "BR-DF", CNTRY: "Brazil", TYPE: "Federal District"
+                }
+                , id:"BR-DF"
+            }
+            ]
+        }
+    }
+}
+
+, ["pqZ3"]);
